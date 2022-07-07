228d129caee8479f92225fee482a81c6a446b112
Jeffrey Stinson <jeffstinson@hotmail.com>
2022-07-07 23:44:51 +0000

    added testing and lint

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,28 @@
 const request = require('request');
-const argv = process.argv.slice(2);
-const breed = argv[0];
-const requestedURL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breed;
 
+const fetchBreedDescription = (breedName, callback) => {
+  // Set url
+  const requestedURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-request(requestedURL, (error, response, body) => {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body);
-    if (data.length === 0) {
-      console.log(`Cat breed '${breed}' does not exist.`);
-    } else {
-      console.log(data[0].description);
-    }
+  if (breedName === undefined) {
+    return callback("Breed name is undefined", null);
   }
-});
+
+  // Request
+  request(requestedURL, (error, response, body) => {
+    
+    if (error) {
+      callback(error, null);
+    } else {
+      const data = JSON.parse(body);
+      
+      if (data.length === 0) {
+        callback(`Cat breed '${breedName}' does not exist.`, null);
+      } else {
+        callback(null, data[0].description);
+      }
+    }
+  });
+};
+
+module.exports = { fetchBreedDescription };
